b4a28eb950dd0fd3964bec19f6faedf2a5ace983
Ali-Altabtabae <78157385+Ali-Altabtabae@users.noreply.github.com>
2021-02-13 14:51:29 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,224 @@
+function reservationMessage1() {
+    alert('Your reservation has been completed!')
+    if(document.getElementById("sel1").value == "t-7-8") {
+        document.getElementById("opt1").disabled = true
+        document.getElementById("sel1").value = "none"
+    } else if(document.getElementById("sel1").value == "t-8-9") {
+        document.getElementById("opt2").disabled = true
+        document.getElementById("sel1").value = "none"
+    } else if(document.getElementById("sel1").value == "t-9-10") {
+        document.getElementById("opt3").disabled = true
+        document.getElementById("sel1").value = "none"
+    } else if(document.getElementById("sel1").value == "t-10-11") {
+        document.getElementById("opt4").disabled = true
+        document.getElementById("sel1").value = "none"
+    } else if(document.getElementById("sel1").value == "t-11-12") {
+        document.getElementById("opt5").disabled = true
+        document.getElementById("sel1").value = "none"
+    } else if(document.getElementById("sel1").value == "t-12-1") {
+        document.getElementById("opt6").disabled = true
+        document.getElementById("sel1").value = "none"
+    } else {
+        document.getElementById("sel1").value = "none"
+    }
+}
+
+function reservationMessage2() {
+    alert('Your reservation has been completed!')
+    if(document.getElementById("sel2").value == "t-7-8") {
+        document.getElementById("opt1_2").disabled = true
+        document.getElementById("sel2").value = "none"
+    } else if(document.getElementById("sel2").value == "t-8-9") {
+        document.getElementById("opt2_2").disabled = true
+        document.getElementById("sel2").value = "none"
+    } else if(document.getElementById("sel2").value == "t-9-10") {
+        document.getElementById("opt3_2").disabled = true
+        document.getElementById("sel2").value = "none"
+    } else if(document.getElementById("sel2").value == "t-10-11") {
+        document.getElementById("opt4_2").disabled = true
+        document.getElementById("sel2").value = "none"
+    } else if(document.getElementById("sel2").value == "t-11-12") {
+        document.getElementById("opt5_2").disabled = true
+        document.getElementById("sel2").value = "none"
+    } else if(document.getElementById("sel2").value == "t-12-1") {
+        document.getElementById("opt6_2").disabled = true
+        document.getElementById("sel2").value = "none"
+    } else {
+        document.getElementById("sel2").value = "none"
+    }
+}
+
+function reservationMessage3() {
+    alert('Your reservation has been completed!')
+    if(document.getElementById("sel3").value == "t-7-8") {
+        document.getElementById("opt1_3").disabled = true
+        document.getElementById("sel3").value = "none"
+    } else if(document.getElementById("sel3").value == "t-8-9") {
+        document.getElementById("opt2_3").disabled = true
+        document.getElementById("sel3").value = "none"
+    } else if(document.getElementById("sel3").value == "t-9-10") {
+        document.getElementById("opt3_3").disabled = true
+        document.getElementById("sel3").value = "none"
+    } else if(document.getElementById("sel3").value == "t-10-11") {
+        document.getElementById("opt4_3").disabled = true
+        document.getElementById("sel3").value = "none"
+    } else if(document.getElementById("sel3").value == "t-11-12") {
+        document.getElementById("opt5_3").disabled = true
+        document.getElementById("sel3").value = "none"
+    } else if(document.getElementById("sel3").value == "t-12-1") {
+        document.getElementById("opt6_3").disabled = true
+        document.getElementById("sel3").value = "none"
+    } else {
+        document.getElementById("sel3").value = "none"
+    }
+}
+
+function reservationMessage4() {
+    alert('Your reservation has been completed!')
+    if(document.getElementById("sel4").value == "t-7-8") {
+        document.getElementById("opt1_4").disabled = true
+        document.getElementById("sel4").value = "none"
+    } else if(document.getElementById("sel4").value == "t-8-9") {
+        document.getElementById("opt2_4").disabled = true
+        document.getElementById("sel4").value = "none"
+    } else if(document.getElementById("sel4").value == "t-9-10") {
+        document.getElementById("opt3_4").disabled = true
+        document.getElementById("sel4").value = "none"
+    } else if(document.getElementById("sel4").value == "t-10-11") {
+        document.getElementById("opt4_4").disabled = true
+        document.getElementById("sel4").value = "none"
+    } else if(document.getElementById("sel4").value == "t-11-12") {
+        document.getElementById("opt5_4").disabled = true
+        document.getElementById("sel4").value = "none"
+    } else if(document.getElementById("sel4").value == "t-12-1") {
+        document.getElementById("opt6_4").disabled = true
+        document.getElementById("sel4").value = "none"
+    } else {
+        document.getElementById("sel4").value = "none"
+    }
+}
+
+function reservationMessage5() {
+    alert('Your reservation has been completed!')
+    if(document.getElementById("sel5").value == "t-7-8") {
+        document.getElementById("opt1_5").disabled = true
+        document.getElementById("sel5").value = "none"
+    } else if(document.getElementById("sel5").value == "t-8-9") {
+        document.getElementById("opt2_5").disabled = true
+        document.getElementById("sel5").value = "none"
+    } else if(document.getElementById("sel5").value == "t-9-10") {
+        document.getElementById("opt3_5").disabled = true
+        document.getElementById("sel5").value = "none"
+    } else if(document.getElementById("sel5").value == "t-10-11") {
+        document.getElementById("opt4_5").disabled = true
+        document.getElementById("sel5").value = "none"
+    } else if(document.getElementById("sel5").value == "t-11-12") {
+        document.getElementById("opt5_5").disabled = true
+        document.getElementById("sel5").value = "none"
+    } else if(document.getElementById("sel5").value == "t-12-1") {
+        document.getElementById("opt6_5").disabled = true
+        document.getElementById("sel5").value = "none"
+    } else {
+        document.getElementById("sel5").value = "none"
+    }
+}
+
+function reservationMessage6() {
+    alert('Your reservation has been completed!')
+    if(document.getElementById("sel6").value == "t-7-8") {
+        document.getElementById("opt1_6").disabled = true
+        document.getElementById("sel6").value = "none"
+    } else if(document.getElementById("sel6").value == "t-8-9") {
+        document.getElementById("opt2_6").disabled = true
+        document.getElementById("sel6").value = "none"
+    } else if(document.getElementById("sel6").value == "t-9-10") {
+        document.getElementById("opt3_6").disabled = true
+        document.getElementById("sel6").value = "none"
+    } else if(document.getElementById("sel6").value == "t-10-11") {
+        document.getElementById("opt4_6").disabled = true
+        document.getElementById("sel6").value = "none"
+    } else if(document.getElementById("sel6").value == "t-11-12") {
+        document.getElementById("opt5_6").disabled = true
+        document.getElementById("sel6").value = "none"
+    } else if(document.getElementById("sel6").value == "t-12-1") {
+        document.getElementById("opt6_6").disabled = true
+        document.getElementById("sel6").value = "none"
+    } else {
+        document.getElementById("sel6").value = "none"
+    }
+}
+
+function reservationMessage7() {
+    alert('Your reservation has been completed!')
+    if(document.getElementById("sel7").value == "t-7-8") {
+        document.getElementById("opt1_7").disabled = true
+        document.getElementById("sel7").value = "none"
+    } else if(document.getElementById("sel7").value == "t-8-9") {
+        document.getElementById("opt2_7").disabled = true
+        document.getElementById("sel7").value = "none"
+    } else if(document.getElementById("sel7").value == "t-9-10") {
+        document.getElementById("opt3_7").disabled = true
+        document.getElementById("sel7").value = "none"
+    } else if(document.getElementById("sel7").value == "t-10-11") {
+        document.getElementById("opt4_7").disabled = true
+        document.getElementById("sel7").value = "none"
+    } else if(document.getElementById("sel7").value == "t-11-12") {
+        document.getElementById("opt5_7").disabled = true
+        document.getElementById("sel7").value = "none"
+    } else if(document.getElementById("sel7").value == "t-12-1") {
+        document.getElementById("opt6_7").disabled = true
+        document.getElementById("sel7").value = "none"
+    } else {
+        document.getElementById("sel7").value = "none"
+    }
+}
+
+function reservationMessage8() {
+    alert('Your reservation has been completed!')
+    if(document.getElementById("sel8").value == "t-7-8") {
+        document.getElementById("opt1_8").disabled = true
+        document.getElementById("sel8").value = "none"
+    } else if(document.getElementById("sel8").value == "t-8-9") {
+        document.getElementById("opt2_8").disabled = true
+        document.getElementById("sel8").value = "none"
+    } else if(document.getElementById("sel8").value == "t-9-10") {
+        document.getElementById("opt3_8").disabled = true
+        document.getElementById("sel8").value = "none"
+    } else if(document.getElementById("sel8").value == "t-10-11") {
+        document.getElementById("opt4_8").disabled = true
+        document.getElementById("sel8").value = "none"
+    } else if(document.getElementById("sel8").value == "t-11-12") {
+        document.getElementById("opt5_8").disabled = true
+        document.getElementById("sel8").value = "none"
+    } else if(document.getElementById("sel8").value == "t-12-1") {
+        document.getElementById("opt6_8").disabled = true
+        document.getElementById("sel8").value = "none"
+    } else {
+        document.getElementById("sel8").value = "none"
+    }
+}
+
+function reservationMessage9() {
+    alert('Your reservation has been completed!')
+    if(document.getElementById("sel9").value == "t-7-8") {
+        document.getElementById("opt1_9").disabled = true
+        document.getElementById("sel9").value = "none"
+    } else if(document.getElementById("sel9").value == "t-8-9") {
+        document.getElementById("opt2_9").disabled = true
+        document.getElementById("sel9").value = "none"
+    } else if(document.getElementById("sel9").value == "t-9-10") {
+        document.getElementById("opt3_9").disabled = true
+        document.getElementById("sel9").value = "none"
+    } else if(document.getElementById("sel9").value == "t-10-11") {
+        document.getElementById("opt4_9").disabled = true
+        document.getElementById("sel9").value = "none"
+    } else if(document.getElementById("sel9").value == "t-11-12") {
+        document.getElementById("opt5_9").disabled = true
+        document.getElementById("sel9").value = "none"
+    } else if(document.getElementById("sel9").value == "t-12-1") {
+        document.getElementById("opt6_9").disabled = true
+        document.getElementById("sel9").value = "none"
+    } else {
+        document.getElementById("sel9").value = "none"
+    }
+}
